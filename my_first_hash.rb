b5e22b10d24df01b5146cb480316ae5cc064f82e
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-27 02:02:08 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+my_hash = {"something" => "of my choice", "when" => "did i get so creative?"}
 end
 
 
